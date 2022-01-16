da66ecd1658a865b21ac8c2b7423bcfb03c3c020
insivika <phil@insivika.com>
2022-01-16 13:57:29 -0600

    convert to class

--- src/client/client.js
+++ src/client/client.js
@@ -3,176 +3,200 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import { JoyStick } from "./libs/toon3d";
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xa0a0a0);
-scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
+class Game {
+  constructor() {
+    this.container;
+    this.player = {};
+    this.stats;
+    this.controls;
+    this.camera;
+    this.scene;
+    this.renderer;
+    this.animations = {};
+    this.animationNames = [
+      "Walking",
+      "Walking Backwards",
+      "Turn",
+      "Running",
+      "Pointing Gesture",
+    ];
+    this.container = document.createElement("div");
+    this.container.style.height = "100%";
+    document.body.appendChild(this.container);
 
-// Camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+    this.assetsPath = "./assets/";
 
-camera.position.set(50, 200, 350);
+    this.clock = new THREE.Clock();
 
-// Clock
-const clock = new THREE.Clock();
+    this.init();
 
-// Render
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.shadowMap.enabled = true;
-document.body.appendChild(renderer.domElement);
-const render = () => {
-  renderer.render(scene, camera);
-};
+    window.onError = function (error) {
+      console.error(JSON.stringify(error));
+    };
+  }
 
-// Controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(0, 150, 0);
-controls.update();
+  init() {
+    this.camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      1,
+      2000
+    );
+    this.camera.position.set(112, 100, 400);
 
-// Hemisphere Light
-let light = new THREE.HemisphereLight(0xffffff, 0x444444);
-light.position.set(0, 200, 0);
-scene.add(light);
-// Directional Light
-light = new THREE.DirectionalLight(0xffffff);
-light.position.set(0, 200, 100);
-light.castShadow = true;
-light.shadow.camera.top = 180;
-light.shadow.camera.bottom = -100;
-light.shadow.camera.left = -120;
-light.shadow.camera.right = 120;
-scene.add(light);
-const ambient = new THREE.AmbientLight(0x707070); // soft white light
-scene.add(ambient);
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xa0a0a0);
+    this.scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
 
-// Ground
-const mesh = new THREE.Mesh(
-  new THREE.PlaneBufferGeometry(2000, 2000),
-  new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
-);
-mesh.rotation.x = -Math.PI / 2;
-mesh.position.y = -100;
-mesh.receiveShadow = true;
-scene.add(mesh);
+    let light = new THREE.HemisphereLight(0xffffff, 0x444444);
+    light.position.set(0, 200, 0);
+    this.scene.add(light);
 
-// Grid Helper
-const grid = new THREE.GridHelper(2000, 40, 0x000000, 0x000000);
-grid.material.opacity = 0.2;
-grid.material.transparent = true;
-scene.add(grid);
+    light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(0, 200, 100);
+    light.castShadow = true;
+    light.shadow.camera.top = 180;
+    light.shadow.camera.bottom = -100;
+    light.shadow.camera.left = -120;
+    light.shadow.camera.right = 120;
+    this.scene.add(light);
 
-// Model
-const player = {};
-const animations = {};
-const animationNames = [
-  "Walking",
-  "Walking Backwards",
-  "Turn",
-  "Running",
-  "Pointing Gesture",
-];
-const loader = new FBXLoader();
-loader.load("./assets/fbx/people/FireFighter.fbx", async (object) => {
-  object.mixer = new THREE.AnimationMixer(object);
+    // ground
+    var mesh = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(2000, 2000),
+      new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
+    );
+    mesh.rotation.x = -Math.PI / 2;
+    mesh.receiveShadow = true;
+    this.scene.add(mesh);
 
-  player.mixer = object.mixer;
-  player.root = object.mixer.getRoot();
+    var grid = new THREE.GridHelper(2000, 40, 0x000000, 0x000000);
+    grid.material.opacity = 0.2;
+    grid.material.transparent = true;
+    this.scene.add(grid);
 
-  object.name = "FireFighter";
+    // model
+    const loader = new FBXLoader();
+    const game = this;
 
-  object.traverse(function (child) {
-    if (child.isMesh) {
-      child.castShadow = true;
-      child.receiveShadow = false;
-    }
-  });
+    loader.load(
+      `${this.assetsPath}fbx/people/FireFighter.fbx`,
+      async (object) => {
+        object.mixer = new THREE.AnimationMixer(object);
+        game.player.mixer = object.mixer;
+        game.player.root = object.mixer.getRoot();
 
-  const tLoader = new THREE.TextureLoader();
+        object.name = "FireFighter";
 
-  tLoader.load(
-    "./assets/images/SimplePeople_FireFighter_Brown.png",
-    (texture) => {
-      object.traverse((child) => {
-        if (child.isMesh) {
-          child.material.map = texture;
-        }
-      });
-    }
-  );
+        object.traverse(function (child) {
+          if (child.isMesh) {
+            child.castShadow = true;
+            child.receiveShadow = false;
+          }
+        });
 
-  scene.add(object);
-  player.object = object;
-  //   Set default animation
-  animations.Idle = object.animations[0];
-
-  await loadAnimations(loader);
-
-  setAnimation();
-
-  //   player.mixer.clipAction(object.animations[0]).play();
-});
-
-const setAction = (name) => {
-  const action = player.mixer.clipAction(animations[name]);
-  action.time = 0;
-  player.mixer.stopAllAction();
-  player.action = name;
-  player.actionTime = Date.now();
-  player.actionName = name;
-  action.fadeIn(0.5);
-  action.play();
-};
-
-const setAnimation = () => {
-  //   createCameras();
-  joystick = new JoyStick({
-    //   onMove: playerControl,
-    //   game: game,
-  });
-  setAction("Idle");
-  animate();
-};
-
-const loadAnimations = async (loader) => {
-  await Promise.all(
-    animationNames.map(async (animationName) => {
-      return new Promise((resolve, reject) => {
-        loader.load(
-          `./assets/fbx/anims/${animationName}.fbx`,
-          function (object) {
-            animations[animationName] = object.animations[0];
-            resolve();
+        const tLoader = new THREE.TextureLoader();
+        tLoader.load(
+          `${game.assetsPath}images/SimplePeople_FireFighter_Brown.png`,
+          function (texture) {
+            object.traverse(function (child) {
+              if (child.isMesh) {
+                child.material.map = texture;
+              }
+            });
           }
         );
-      });
-    })
-  );
-};
 
-// Handle Resizing Window
-const onWindowResize = () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
-};
-window.addEventListener("resize", onWindowResize, false);
+        game.scene.add(object);
+        game.player.object = object;
+        game.animations.Idle = object.animations[0];
 
-const animate = () => {
-  const dt = clock.getDelta();
+        await game.loadAnimations(loader);
 
-  requestAnimationFrame(animate);
+        console.log(game.animations);
 
-  if (player.mixer !== undefined) {
-    player.mixer.update(dt);
+        game.loadInitialAction();
+      }
+    );
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.shadowMap.enabled = true;
+    this.container.appendChild(this.renderer.domElement);
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.target.set(0, 150, 0);
+    this.controls.update();
+
+    window.addEventListener(
+      "resize",
+      function () {
+        game.onWindowResize();
+      },
+      false
+    );
   }
-  render();
-};
 
-animate();
+  loadAnimations = async (loader) => {
+    const game = this;
+    await Promise.all(
+      game.animationNames.map(async (animationName) => {
+        return new Promise((resolve, reject) => {
+          loader.load(
+            `./assets/fbx/anims/${animationName}.fbx`,
+            function (object) {
+              game.animations[animationName] = object.animations[0];
+              resolve();
+            }
+          );
+        });
+      })
+    );
+  };
+
+  setAction = (name) => {
+    const action = this.player.mixer.clipAction(this.animations[name]);
+    action.time = 0;
+    this.player.mixer.stopAllAction();
+    this.player.action = name;
+    this.player.actionTime = Date.now();
+    this.player.actionName = name;
+
+    action.fadeIn(0.5);
+    action.play();
+  };
+
+  getAction = () => {
+    if (this.player === undefined || this.player.actionName === undefined)
+      return "";
+    return this.player.actionName;
+  };
+
+  loadInitialAction = () => {
+    this.setAction("Idle");
+    this.animate();
+  };
+
+  onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  animate() {
+    const game = this;
+    const dt = this.clock.getDelta();
+
+    requestAnimationFrame(function () {
+      game.animate();
+    });
+
+    if (this.player.mixer !== undefined) this.player.mixer.update(dt);
+
+    this.renderer.render(this.scene, this.camera);
+  }
+}
+
+new Game();
